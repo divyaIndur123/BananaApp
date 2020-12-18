ee90ea0070e9e40eba710eae9fe4fc37c2caae9f
Divya indur <divyaindur@gmail.com>
2020-12-18 19:53:46 +0530

    docs:added comment

--- script/javascript.js
+++ script/javascript.js
@@ -1,4 +1,4 @@
-/* wiring click button*/
+/* input*/
 var translateButton = document.querySelector(".button-primary");
 var outputText = document.querySelector(".content-area.main-output");
 var inputText = document.querySelector("textarea");
@@ -6,14 +6,17 @@ var inputText = document.querySelector("textarea");
 //var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 var serverUrl = "https://api.funtranslations.com/translate/minion.json";
 
+//building url with message to translate
 function buildUrl(textInput) {
   return serverUrl + "?" + "text=" + textInput;
 }
 
+// to handle the  server error 
 function errorHandler(error){
     console.log("Server is Down! Try after some time");
 }
 
+// processing - to fetch the data from server
 function clickHandler() {
   var input = inputText.value;
   fetch(buildUrl(input))
@@ -21,4 +24,5 @@ function clickHandler() {
     .then((json) => (outputText.innerText = json.contents.translated))
     .catch(errorHandler);
 }
+
  translateButton.addEventListener("click", clickHandler);
